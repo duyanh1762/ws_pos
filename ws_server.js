30e638a03509d8ef84d46066ccfebfcc1aa10915
Duy Anh <101709779+duyanh1762@users.noreply.github.com>
2024-10-15 09:27:10 +0700

    Update ws_server.js

--- ws_server.js
+++ ws_server.js
@@ -4,13 +4,11 @@ const wss = new WebSocket.Server({ port: 56001 });
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  // Nhận tin nhắn từ thiết bị thu ngân
   ws.on('message', (message) => {
     console.log(`Received order: ${message}`);
-    // Gửi thông báo tới tất cả các client (bao gồm bartender)
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message); // Gửi đơn hàng tới bartender
+        client.send(message);
       }
     });
   });
